26ab57a0d9e36b220a3cfd810663fd3c77c197c2
SangHyun Byun <egoist12276@gmail.com>
2025-04-27 23:41:15 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
 import itertools
+import numpy as np
+import torch
+import timm
+
+def rand_bbox(size, lam):
+    W, H = size[2], size[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w, cut_h = int(W * cut_rat), int(H * cut_rat)
+    cx, cy = np.random.randint(W), np.random.randint(H)
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+    return bbx1, bby1, bbx2, bby2
 
 # 상위 k개 혼동 페어 추출 함수
 def get_topk_confusing_pairs(y_true, y_pred, class_names, topk=5) -> list:
@@ -16,3 +30,11 @@ def get_topk_confusing_pairs(y_true, y_pred, class_names, topk=5) -> list:
     pair_scores.sort(key=lambda x: x[1], reverse=True)
     top_pairs = pair_scores[:topk]
     return top_pairs
+
+# 모델 로딩 함수
+def load_model(model_name, checkpoint_path, num_classes=7):
+    model = timm.create_model(model_name, pretrained=False, num_classes=num_classes)
+    model.load_state_dict(torch.load(checkpoint_path, map_location='cuda')['model_state_dict'])
+    model.to('cuda')
+    model.eval()
+    return model
